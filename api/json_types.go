273e619f93300ff6f7c8a6ad3fc278c52d53445f
tomaszmj <tomasz.m.j.nowak@gmail.com>
2020-08-20 01:20:29 +0200

    Add api/json_types with unit tests

---
+++ api/json_types.go
@@ -0,0 +1,82 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"time"
+)
+
+// Returned by PostNewUrl
+type UrlId struct {
+	Id int `json:"id"`
+}
+
+// Request body in PostNewUrl
+type NewUrl struct {
+	Url      *url.URL `json:"url"`
+	Interval int      `json:"interval"`
+}
+
+// Returned by GetAllUrls
+type ReturnedUrl struct {
+	Id          int    `json:"id"`
+	UrlAsString string `json:"url"`
+	Interval    int    `json:"interval"`
+}
+
+// Returned by GetFetcherHistory
+type UrlResponse struct {
+	Response  *string       `json:"response"`
+	Duration  time.Duration `json:"duration"`
+	CreatedAt time.Time     `json:"created_at"`
+}
+
+func (n *NewUrl) UnmarshalJSON(j []byte) error {
+	var rawData map[string]interface{}
+	err := json.Unmarshal(j, &rawData)
+	if err != nil {
+		return err
+	}
+	if len(rawData) != 2 {
+		return fmt.Errorf("expected exactly 2 keys: url, interval, got %d in json %s", len(rawData), j)
+	}
+	for key, value := range rawData {
+		if key == "url" {
+			urlStr, ok := value.(string)
+			if !ok {
+				return fmt.Errorf("unexpected value for key url (expected url as string, got %v as %T) in json %s", value, value, j)
+			}
+			n.Url, err = url.ParseRequestURI(urlStr)
+			if err != nil {
+				return err
+			}
+		} else if key == "interval" {
+			intervalFloat, ok := value.(float64)
+			if !ok {
+				return fmt.Errorf("unexpected value for key interval (expected number, got %v as %T) in json %s", value, value, j)
+			}
+			n.Interval = int(intervalFloat)
+			if float64(n.Interval) != intervalFloat {
+				return fmt.Errorf("invalid interval in new url - must be positive integer, got %f", intervalFloat)
+			}
+			if n.Interval <= 0 {
+				return fmt.Errorf("invalid interval in new url - must be positive integer, got %d", n.Interval)
+			}
+		}
+	}
+	return nil
+}
+
+func (u *UrlResponse) MarshalJSON() ([]byte, error) {
+	base := struct {
+		Response  *string `json:"response"`
+		Duration  float64 `json:"duration"`
+		CreatedAt int64   `json:"created_at"`
+	}{
+		Response:  u.Response,
+		Duration:  u.Duration.Seconds(),
+		CreatedAt: u.CreatedAt.Unix(),
+	}
+	return json.Marshal(base)
+}
---
+++ api/json_types_test.go
@@ -0,0 +1,97 @@
+package api_test
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/url"
+	"testing"
+	"time"
+
+	"fetcher/api"
+)
+
+// Tests cover only API use cases, for example NewUrl is used as response body in PostNewUrl, so there is only Unmarshal
+func TestJsonMarshalling(t *testing.T) {
+	t.Run("Marshal UrlId", func(t *testing.T) {
+		bytes, err := json.Marshal(api.UrlId{Id: 2})
+		require.NoError(t, err)
+		assert.Equal(t, `{"id":2}`, string(bytes))
+	})
+
+	t.Run("Unmarshal NewUrl", func(t *testing.T) {
+		t.Run("with valid json", func(t *testing.T) {
+			data := []byte(`{"url":"https://httpbin.org/range/15","interval":60}`)
+			expectedUrl, err := url.Parse("https://httpbin.org/range/15")
+			require.NoError(t, err)
+			var newUrl api.NewUrl
+			require.NoError(t, json.Unmarshal(data, &newUrl))
+			assert.Equal(t, api.NewUrl{Url: expectedUrl, Interval: 60}, newUrl)
+		})
+		t.Run("with invalid json syntax", func(t *testing.T) {
+			data := []byte(`{"url":"https://httpbin.org/range/15","interval":60`)
+			var newUrl api.NewUrl
+			assert.Error(t, json.Unmarshal(data, &newUrl))
+		})
+		t.Run("with invalid url", func(t *testing.T) {
+			data := []byte(`{"url":"xx","interval":60}`)
+			var newUrl api.NewUrl
+			assert.Error(t, json.Unmarshal(data, &newUrl))
+		})
+		t.Run("with non-number interval", func(t *testing.T) {
+			data := []byte(`{"url":"https://httpbin.org/range/15","interval":"60"}`)
+			var newUrl api.NewUrl
+			assert.Error(t, json.Unmarshal(data, &newUrl))
+		})
+		t.Run("with non-integer interval", func(t *testing.T) {
+			data := []byte(`{"url":"https://httpbin.org/range/15","interval":60.5}`)
+			var newUrl api.NewUrl
+			assert.Error(t, json.Unmarshal(data, &newUrl))
+		})
+		t.Run("with negative interval", func(t *testing.T) {
+			data := []byte(`{"url":"https://httpbin.org/range/15","interval":-1}`)
+			var newUrl api.NewUrl
+			assert.Error(t, json.Unmarshal(data, &newUrl))
+		})
+		t.Run("with missing json key", func(t *testing.T) {
+			data := []byte(`{"url":"https://httpbin.org/range/15"}`)
+			var newUrl api.NewUrl
+			assert.Error(t, json.Unmarshal(data, &newUrl))
+		})
+		t.Run("with unexpected json key", func(t *testing.T) {
+			data := []byte(`{"url":"https://httpbin.org/range/15","interval":60,"key":"value"}`)
+			var newUrl api.NewUrl
+			assert.Error(t, json.Unmarshal(data, &newUrl))
+		})
+	})
+
+	t.Run("Marshal ReturnedUrl", func(t *testing.T) {
+		bytes, err := json.Marshal(api.ReturnedUrl{Id: 11, UrlAsString: "https://httpbin.org/range/15", Interval: 60})
+		require.NoError(t, err)
+		assert.Equal(t, `{"id":11,"url":"https://httpbin.org/range/15","interval":60}`, string(bytes))
+	})
+
+	t.Run("Marshal UrlResponse", func(t *testing.T) {
+		t.Run("with non-empty response", func(t *testing.T) {
+			responseStr := "abcd"
+			urlResponse := api.UrlResponse{
+				Response:  &responseStr,
+				Duration:  time.Duration(int64(0.571 * float64(time.Second))),
+				CreatedAt: time.Unix(1559034638, 0),
+			}
+			bytes, err := json.Marshal(&urlResponse)
+			require.NoError(t, err)
+			assert.Equal(t, `{"response":"abcd","duration":0.571,"created_at":1559034638}`, string(bytes))
+		})
+		t.Run("with empty response", func(t *testing.T) {
+			urlResponse := api.UrlResponse{
+				Response:  nil,
+				Duration:  time.Duration(int64(0.571 * float64(time.Second))),
+				CreatedAt: time.Unix(1559034638, 0),
+			}
+			bytes, err := json.Marshal(&urlResponse)
+			require.NoError(t, err)
+			assert.Equal(t, `{"response":null,"duration":0.571,"created_at":1559034638}`, string(bytes))
+		})
+	})
+}
