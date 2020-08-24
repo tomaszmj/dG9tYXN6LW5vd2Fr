ae9ea5f7699d48a9b9b4d236757635fcf386ec24
tomaszmj <tomasz.m.j.nowak@gmail.com>
2020-08-24 05:35:03 +0200

    Add worker which fetches urls in the background

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -9,22 +9,17 @@ import (
 
 	"fetcher/api"
 	"fetcher/urls"
+	"fetcher/worker"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	api.Create(r, urls.New(&fakeWorker{}))
+	api.Create(r, urls.New(worker.New()))
 	fmt.Println("Starting server on port 8080 ...")
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		fmt.Println("Failed to start server on port 8080:", err)
 		return
 	}
-}
-
-type fakeWorker struct {
-}
-
-func (f *fakeWorker) NewFetchRoutine(newUrl api.NewUrl, onFetch func(response api.UrlResponse), stopChan chan struct{}) {
 }
---
+++ worker/worker.go
@@ -0,0 +1,95 @@
+package worker
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	"fetcher/api"
+)
+
+type Worker struct {
+}
+
+const httpRequestTimeout = 5
+const abortSendingResponseTimeout = 10
+
+type urlResponseWithError struct {
+	Response api.UrlResponse
+	Error    error
+}
+
+func New() *Worker {
+	return &Worker{}
+}
+
+func (w *Worker) NewFetchRoutine(url api.NewUrl, onFetch func(response api.UrlResponse), stopChan chan struct{}) {
+	go func() {
+		responseChan := make(chan urlResponseWithError)
+		ticker := time.NewTicker(time.Duration(url.IntervalSeconds) * time.Second)
+		for {
+			select {
+			case <-stopChan:
+				ticker.Stop()
+				return
+			case response := <-responseChan:
+				if response.Error != nil {
+					log.Println(response.Error)
+				} else {
+					onFetch(response.Response)
+				}
+			case <-ticker.C:
+				go makeRequestAndSaveResponse(url, responseChan)
+			}
+		}
+	}()
+}
+
+func makeRequestAndSaveResponse(url api.NewUrl, responseChan chan urlResponseWithError) {
+	response, err := makeHttpRequest(url.Url)
+	// Set timeout on writing response to avoid leaking goroutines in case url gets deleted, i.e.
+	// fetcher routine reads from stopChan and exits without reading response from this function
+	timer := time.NewTimer(abortSendingResponseTimeout * time.Second)
+	select {
+	case responseChan <- urlResponseWithError{Response: response, Error: err}:
+		timer.Stop()
+		return
+	case <-timer.C:
+		return
+	}
+}
+
+func makeHttpRequest(url *url.URL) (api.UrlResponse, error) {
+	createdAt := time.Now()
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, httpRequestTimeout*time.Second)
+	defer cancel()
+	request, err := http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+	if err != nil {
+		e := fmt.Errorf("could not create request for url %s: %s", url.String(), err)
+		return api.UrlResponse{}, e
+	}
+	t1 := time.Now()
+	response, err := http.DefaultClient.Do(request)
+	t2 := time.Now()
+	duration := t2.Sub(t1)
+	if errors.Is(err, context.DeadlineExceeded) {
+		return api.UrlResponse{Response: nil, Duration: duration, CreatedAt: createdAt}, nil
+	} else if err != nil {
+		return api.UrlResponse{}, fmt.Errorf("unexpected error on fetching url %s: %s", url.String(), err)
+	}
+	if response.StatusCode != http.StatusOK {
+		return api.UrlResponse{Response: nil, Duration: duration, CreatedAt: createdAt}, nil
+	}
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return api.UrlResponse{Response: nil, Duration: duration, CreatedAt: createdAt}, nil
+	}
+	responseStr := string(bytes)
+	return api.UrlResponse{Response: &responseStr, Duration: duration, CreatedAt: createdAt}, nil
+}
